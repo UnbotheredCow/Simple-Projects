9841f81fb02739538385d21a63cd870753e6d721
Swarnim Chaudhary <swarnim.unofficial@gmail.com>
2026-02-08 04:15:50 +0530

    Completed Quiz Game

--- Quiz-Game/script.js
+++ Quiz-Game/script.js
@@ -17,7 +17,7 @@ const scoreElement = document.getElementById("Score");
 const answerContainer = document.getElementById("answerContainer");
 
 // PROGRESS BAR
-const progressBar = document.getElementById("progres");
+const progressBar = document.getElementById("progress");
 
 // RESULT
 const correctAnswers = document.getElementById("correctAnswers");
@@ -119,4 +119,115 @@ const quizQuestions = [
     }
 ];
 
+// QUIZ STATE VARIABLES
+
+let currentQuestionIndex = 0;
+let score = 0;
+let answersDisabled = false
+
+totalQuestion.textContent = quizQuestions.length
+maxScore.textContent = quizQuestions.length
+
+
+//EVENT LISTNERS
+startButton.addEventListener("click", startQuiz)
+restartButton.addEventListener("click", restartQuiz)
+
+function startQuiz(){
+    //Reset Vars
+    currentQuestionIndex = 0;
+    score = 0;
+    scoreElement.textContent = score
+
+    startScreen.classList.remove("active")
+    quizScreen.classList.add("active")  
+
+    showQuestion()
+}
+
+function restartQuiz(){
+    resultScreen.classList.remove("active")
+    startQuiz()
+}
+
+function showQuestion(){
+    //RESET STATE
+    answersDisabled = false
+
+    const questionData = quizQuestions[currentQuestionIndex];
+    currentQuestion.textContent = currentQuestionIndex + 1;
+    const progresPercent = (currentQuestionIndex/quizQuestions.length)*100;
+
+    progressBar.style.width = progresPercent + "%"
+    questionText.textContent = questionData.question
+
+    answerContainer.innerHTML = "";
+
+    questionData.answers.forEach(answers => {
+        const button = document.createElement("button")
+        button.textContent = answers.text
+        button.classList.add("answerButton")
+
+        button.dataset.correct = answers.correct
+
+        button.addEventListener("click", selectAnswer);
+        answerContainer.appendChild(button)
+    })
+}
+
+function selectAnswer(event){
+    if(answersDisabled == true) return
+
+    answersDisabled = true
+    const selectedButton = event.target;
+    const isCorrect = selectedButton.dataset.correct === "true"
+
+    Array.from(answerContainer.children).forEach(button => {
+        if(button.dataset.correct == 'true'){
+            button.classList.add("correct")
+        }
+        else if (button === selectedButton){
+            button.classList.add("incorrect")
+        }
+    })
+
+    if(isCorrect){
+        score++;
+        scoreElement.textContent = score;
+
+    }
+
+    setTimeout(()=>{
+        currentQuestionIndex++;
+        //CHECK FOR MORE QUESTION
+        if(currentQuestionIndex < quizQuestions.length){
+            showQuestion();
+        }else{
+            showResult()
+        }
+    }, 1000)
+}
+
+function showResult(){
+    quizScreen.classList.remove("active")
+    resultScreen.classList.add("active")
+
+    correctAnswers.textContent = score;
+    const percentage = (score / quizQuestions.length) * 100;
+
+    if (percentage === 100) {
+        resultMessage.textContent = "You are a GENIUS!!";
+    } else if (percentage >= 80) {
+        resultMessage.textContent = "Excellent work! You're really smart";
+    } else if (percentage >= 60) {
+        resultMessage.textContent = "Good job! You know your stuff";
+    } else if (percentage >= 40) {
+        resultMessage.textContent = "Not bad! Keep practicing";
+    } else if (percentage >= 20) {
+        resultMessage.textContent = "You can do better! Try again";
+    } else {
+        resultMessage.textContent = "Oof!! Time to study and retry!";
+    }
+
+}
 
